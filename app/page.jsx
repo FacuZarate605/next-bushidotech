0a4e0ef1e520c034d8465434252b584814dbb3af
Facundo Zárate <157718085+FacuZarate605@users.noreply.github.com>
2024-10-06 01:26:35 -0300

    Update page.jsx
    ¡Recomendamos este increíble sitio web de tecnología! Te esperamos:"

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ The card below is rendered on the server based on the value of \`process.env.CON
 `;
 
 const preDynamicContentExplainer = `
-The card content below is fetched by the client-side from \`/quotes/random\` (see file \`app/quotes/random/route.js\`) with a different quote shown on each page load:
+¡Recomendamos este increíble sitio web de tecnología! Te esperamos:"
 `;
 
 const postDynamicContentExplainer = `
@@ -55,7 +55,7 @@ export default function Page() {
             {/* !!cards?.length && <CardsGrid cards={cards} /> */}
         </main>
     );
-}
+}    
 
 function RuntimeContextCard() {
     const title = `Netlify Context: running in ${ctx} mode.`;
